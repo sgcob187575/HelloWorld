5c6a11c917b07f1806e7c25349f0c56a10c567cd
Spencer Chen <spencer.chen@getac.com.tw>
2020-07-10 08:53:30 +0800

    allowProvisioningupdate

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!\ntestfortesmcity")
+        Text("Hello, World!\ntestfortesmcity\nstep2")
     }
 }
 
